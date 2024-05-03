7a04bbff82af70cb0feb64c7f7fba415b32f2a9a
Biro Armand <biro.armand@gmail.com>
2024-05-03 13:51:41 +0200

    top 5 könvy a főoladlon

--- app/products.php
+++ app/products.php
@@ -1,9 +1,34 @@
 <?php
 include "scripts/getAllProducts.php";
+include "scripts/topProducts.php";
 ?>
 
 <div class="container mt-5">
-    <h1 class="mb-4">Könyveink</h1>
+    <h1 class="mb-4">Legnépszerűbb könyveink</h1>
+    <div class="row">
+
+    <?php
+        while (($row_top = oci_fetch_array($top_cursor, OCI_ASSOC+OCI_RETURN_NULLS)) !== false) {
+        ?>
+            <div class="col-md-2 mb-4" style="margin-left: auto; margin-right: auto;">
+                <div class="card h-100 text-center">
+                    <img src="assets/productImages/literally.jpg" class="card-img-top productImage" alt="könyv kép" style="max-width: 100%; display: block; margin-left: auto; margin-right: auto;">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="?page=item&id=<?php echo $row_top["KONYV_ID"] ?>"><?php echo $row_top["KONYV_CIM"]; ?></a></h5>
+                        <p class="card-text"><?php echo $row_top["SZERZO_NEV"]; ?></p>
+                        <p class="card-text" style="font-size: 1.3rem"><strong><?php echo $row_top["AR"] . ' Ft'; ?></strong></p>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+    ?>
+
+
+
+    </div>
+
+    <h1 class="mt-5 mb-4">Könyveink</h1>
     <div class="row">
         <?php
             while (($row = oci_fetch_array($cursor, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
